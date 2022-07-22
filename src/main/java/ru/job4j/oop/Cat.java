33aa4ae50017d38233aed048e72509ab8d34c8d1
LESHA <belochuvach@gmail.com>
2022-07-22 17:15:32 +0300

    Поля объекта

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String name;
+
+    private String food;
+
+    public void nick() {
+        System.out.println(this.name);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
+        Cat num = new Cat();
+        num.giveNick("Barsik");
+        num.nick();
+        num.eat("chicken");
+        num.show();
+    }
+}
