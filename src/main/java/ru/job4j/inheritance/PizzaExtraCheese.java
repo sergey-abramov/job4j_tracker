b09e3124b5499c1f84637f653ba3cd4ae615f75c
LESHA <belochuvach@gmail.com>
2022-07-27 00:13:27 +0300

    Вызов переопределенного метода родителя - super.method

--- src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -1,9 +1,8 @@
 package ru.job4j.inheritance;
 
 public class PizzaExtraCheese extends Pizza {
-    public final String name1 = "+ Extra Cheese ";
 
     public String name() {
-        return super.name() + name1;
+        return super.name() + "+ Extra Cheese ";
     }
 }
--- src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -1,10 +1,9 @@
 package ru.job4j.inheritance;
 
 public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
-    public final String name2 = "+ ExtraTomato ";
 
     public String name() {
-        return super.name() + name2;
+        return super.name() + "+ ExtraTomato ";
     }
 }
 
