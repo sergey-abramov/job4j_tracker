91be8835d70e48da056ed0921da0e6f2ce37582d
LESHA <belochuvach@gmail.com>
2022-07-27 12:58:46 +0300

    Модель данных.

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Петров Иван Николаевич");
+        student.setGroup("Водные пути");
+        student.setCreated(new Date());
+
+        System.out.println(student.getName() + " group : " + student.getGroup() + " date : " + student.getCreated());
+    }
+}
