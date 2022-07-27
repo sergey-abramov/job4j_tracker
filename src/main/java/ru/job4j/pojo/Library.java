2278637358e3b1f25f3e25347cb98c63523c3e33
LESHA <belochuvach@gmail.com>
2022-07-27 14:20:30 +0300

    Массивы и модели

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book wow = new Book("WoW", 716);
+        Book metro2032 = new Book("Metro2032", 1025);
+        Book java8 = new Book("Java8", 712);
+        Book cleancode = new Book("Clean code", 1245);
+        Book[] book = new Book[4];
+        book[0] = wow;
+        book[1] = metro2032;
+        book[2] = java8;
+        book[3] = cleancode;
+        for (int i = 0; i < book.length; i++) {
+            Book bk = book[i];
+            System.out.println(bk.getName() + " number of pages :" + bk.getNumber());
+        }
+
+        System.out.println("Book 1 = Clean code; book 4 = WoW");
+        book[0] = cleancode;
+        book[3] = wow;
+        for (int i = 0; i < book.length; i++) {
+            Book bk = book[i];
+            System.out.println(bk.getName() + " number of pages :" + bk.getNumber());
+        }
+
+        System.out.println("Book only Clean code");
+        for (int i = 0; i < book.length; i++) {
+            Book bk = book[i];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " number of pages :" + bk.getNumber());
+            }
+        }
+    }
+}
