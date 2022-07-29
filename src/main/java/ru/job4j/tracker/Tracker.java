a2c64892c980adefa268372451b423c6b1d7f795
LESHA <belochuvach@gmail.com>
2022-07-29 22:19:57 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                rsl[index] = item;
+                size--;
+                break;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        for (int index = 1; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl[index] = item;
+                size -= index;
+                break;
+            }
+        }
+       return Arrays.copyOf(rsl, size);
     }
 }
