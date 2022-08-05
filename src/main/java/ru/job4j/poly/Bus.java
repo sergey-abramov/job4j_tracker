aeecb4e4ecce661c4cb8a44094c763d01abb9926
LESHA <belochuvach@gmail.com>
2022-08-05 23:24:36 +0300

    Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+﻿package ru.job4j.poly;
+
+public class Bus extends Transport {
+    @Override
+    boolean drive() {
+        return false;
+    }
+
+    @Override
+    int passengers(int passengers) {
+        return passengers;
+    }
+
+    @Override
+    int gas(int gas, int price) {
+        return gas / price;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+﻿package ru.job4j.poly;
+
+public abstract class Transport {
+    abstract boolean drive();
+
+    abstract int passengers(int passengers);
+
+    abstract int gas(int gas, int price);
+}
