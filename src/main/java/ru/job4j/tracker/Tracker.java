229a92e63a3aecc31a55674fbef344f84da2c5d8
LESHA <belochuvach@gmail.com>
2022-09-13 21:45:46 +0300

    Изменить программу Tracker из 2-го модуля

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,32 +1,33 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final List<Item> items = new ArrayList<>(100);
     private int ids = 1;
-    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        return items.toArray(new Item[0]);
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[items.length];
+        Item[] rsl = new Item[items.size()];
         int count = 0;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int index = 0; index < items.size(); index++) {
+            Item item = items.get(index);
             if (item.getName().equals(key)) {
                 rsl[count++] = item;
             }
@@ -36,8 +37,8 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
             }
@@ -50,7 +51,7 @@ public class Tracker {
         boolean rslValidation = index != -1;
         if (rslValidation) {
             item.setId(id);
-            items[index] = item;
+            items.set(index, item);
         }
         return rslValidation;
     }
@@ -59,9 +60,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rslValidation = index != -1;
         if (rslValidation) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
+            items.remove(index);
         }
         return rslValidation;
     }
