90c4c634630b05b41813bcca0185d6179793880f
LESHA <belochuvach@gmail.com>
2022-09-21 23:32:36 +0300

    Организовать сортировку User

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,58 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        int rsl = this.name.compareTo(user.name);
+        if (this.name.equals(user.name)) {
+            rsl = this.age - user.age;
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.collection;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next()).isEqualTo(new User("Ivan", 31));
+        assertThat(it.next()).isEqualTo(new User("Petr", 32));
+    }
+
+    @Test
+    public void whenAscForUsersWithEqualNameButDifferentAge() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 35));
+        users.add(new User("Ivan", 31));
+        users.add(new User("Petr", 25));
+        users.add(new User("Ivan", 29));
+        users.add(new User("Petr", 33));
+        users.add(new User("Ivan", 27));
+        Set<User> expected = new TreeSet<>(
+                List.of(
+                        new User("Ivan", 27),
+                        new User("Ivan", 29),
+                        new User("Ivan", 31),
+                        new User("Petr", 25),
+                        new User("Petr", 33),
+                        new User("Petr", 35)
+                )
+        );
+        assertThat(users).containsAll(expected);
+    }
+
+    @Test
+    public void whenComparePetrVSIvan() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Ivan", 31)
+                );
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenComparePetrVSPetrWithDifferentAge() {
+        int rsl = new User("Petr", 31)
+                .compareTo(
+                        new User("Petr", 32)
+                );
+        assertThat(rsl).isLessThan(0);
+    }
+}
