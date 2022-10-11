0f7932fb7e71385fb69feb33f86e4bcbfca78adb
LESHA <belochuvach@gmail.com>
2022-10-11 12:26:42 +0300

    Local-Variable Type Inference

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,35 +10,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
-        ArrayList<Person> findPerson = new ArrayList<>();
+        var persons = phones.find("Petr");
+        var findPerson = new ArrayList<>();
         findPerson.addAll(persons);
         assertThat(persons).isEqualTo(findPerson);
     }
 
     @Test
     public void whenFindByAddress() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Kislovodsk");
-        ArrayList<Person> findPerson = new ArrayList<>();
+        var persons = phones.find("Kislovodsk");
+        var findPerson = new ArrayList<>();
         findPerson.addAll(persons);
         assertThat(persons).isEqualTo(findPerson);
     }
 
     @Test
     public void whenFindByName1() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 }
