030e2b7a8c97950d3181bc4d6e18ea539aff3c6d
LESHA <belochuvach@gmail.com>
2022-10-12 20:13:35 +0300

    Метод .of

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -10,7 +10,7 @@ class DepartmentsTest {
 
     @Test
     public void whenMissed() {
-        List<String> input = List.of("k1/sk1", "k2/sk1");
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
         List<String> expect = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsAll(expect);
@@ -18,7 +18,7 @@ class DepartmentsTest {
 
     @Test
     public void whenNonChange() {
-        List<String> input = List.of("k1", "k1/sk1");
+        List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsAll(expect);
@@ -37,7 +37,7 @@ class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -62,7 +62,7 @@ class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K1/SK2",
@@ -87,7 +87,7 @@ class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
@@ -113,7 +113,7 @@ class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2/SK1",
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2",
